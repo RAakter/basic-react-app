4228a9ebe00204b1d223940db6b262280720e6d9
Rezowana Akter <56617322+RAakter@users.noreply.github.com>
2023-01-11 19:53:39 +0600

    T4: JSX and JS expression

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name = "Rezowana Akter";
+const date = new Date();
+const dateName = date.getDate();
+const monthName = date.getMonth() + 1;
+const yearName = date.getFullYear();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <h1>Welcome to my React App</h1>
+    <p>{name}</p>
+    <p>Started on: {dateName + "/0" +monthName+ "/" + yearName}</p>
+    <p>{}</p>
+    <p>{}</p>
+  </div>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
