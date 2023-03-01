d3e6b6ce780fe5363443af333bc6fb7e2a6dd25d
Rezowana Akter <56617322+RAakter@users.noreply.github.com>
2023-03-01 19:44:10 +0600

    T8: Props and destructuring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Card from './Components/Card';
 
 function App() {
-  return "App";
+    return <div>
+      <h1 className='headingStyle'>Todo App</h1>
+      <Card titleTxt="My Card 1" desText="First Description"/>
+      <Card titleTxt="My Card 2" desText="Second Description"/>
+      <Card titleTxt="My Card 3" desText="Third Description"/>
+    </div>
 }
 
 export default App;
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,11 +5,16 @@ const dateName = date.getDate();
 const monthName = date.getMonth() + 1;
 const yearName = date.getFullYear();
 
-function Card(){
+function Card(props){
+    const {titleTxt, desText} = props;
     return <div className='card'>
-    <p className='cardTitle'>{name}</p>
+      <p className='cardTitle'>{titleTxt}</p>
+      <p className='cardDes'>{desText}</p>
+      <p className='cardDes'>Started on: {dateName + "/0" +monthName+ "/" + yearName}</p>
+    
+    {/* <p className='cardTitle'>{name}</p>
     <p className='cardDes'>Started on: {dateName + "/0" +monthName+ "/" + yearName}</p>
-    <p className='cardFooter'>{name}</p>
+    <p className='cardFooter'>{name}</p> */}
   </div>
  }
 
