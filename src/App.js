0874b1fafe95df614704dada83c1edbe7d966f0f
Rezowana Akter <56617322+RAakter@users.noreply.github.com>
2023-03-01 20:04:49 +0600

    T9 : Mapping data to components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Card from './Components/Card';
+import Data from './data.json'
 
 function App() {
+  // console.log(Data);
+  let items = [];
+
+  // for(let x=0; x<Data.length; x++){
+  //   items.push( <Card titleTxt={Data[x].title} desText={Data[x].desc}/>)
+  // }
+
+  //data mapping
+   items = Data.map((item, index)=> <Card key={index} titleTxt={item.title} desText={item.desc}/>)
     return <div>
       <h1 className='headingStyle'>Todo App</h1>
-      <Card titleTxt="My Card 1" desText="First Description"/>
+      {/* <Card titleTxt="My Card 1" desText="First Description"/>
       <Card titleTxt="My Card 2" desText="Second Description"/>
-      <Card titleTxt="My Card 3" desText="Third Description"/>
+      <Card titleTxt="My Card 3" desText="Third Description"/> */}
+      {items}
     </div>
 }
 
